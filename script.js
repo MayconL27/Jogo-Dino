f041c188cc3c9d2c7ca7f2c1d8f998618edc02f4
MayconL27 <mayconleandro2008@gmail.com>
2022-04-25 10:47:26 -0300

    Pulo melhorado

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let personagem = document.querySelector('#personagem')
 let quadrado = document.querySelector('#quadrado')
 alert('iniciar')
 function pular() {
+    // Adicionar classe .animar
     if(personagem.classList != 'animar'){
         personagem.classList.add('animar')
     }
 
+    // Pausar pulo
     setTimeout(function(){
         personagem.classList.remove('animar')
     }, 500)
