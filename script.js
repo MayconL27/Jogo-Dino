975700e80a429326efcbef8b118d0169c9bfbf18
MayconL27 <mayconleandro2008@gmail.com>
2022-04-25 10:54:35 -0300

    Event onKeypess

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let personagem = document.querySelector('#personagem')
 let quadrado = document.querySelector('#quadrado')
 alert('iniciar')
+
 function pular() {
     // Adicionar classe .animar
     if(personagem.classList != 'animar'){
